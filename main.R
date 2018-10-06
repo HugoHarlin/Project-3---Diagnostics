235d9de2839ff8018caaac4607b7e7fa77787fca
HugoHarlin <36413511+HugoHarlin@users.noreply.github.com>
2018-10-06 15:19:24 +0200

    dilch

--- main.R
+++ main.R
@@ -2,6 +2,9 @@ rm(list = ls())
 setwd("~/Teknisk Fysik/AI/Project-3-Diagnostics")
 #setwd("~/Teknisk Fysik/AI/Project-3-Diagnostics")
 library(Diagnostics)
+source("diagnose.R")
+source("learn.R")
+runDiagnostics(learn, diagnose, verbose = 1)
 
 
 
